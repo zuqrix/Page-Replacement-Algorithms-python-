9ce1cce65267119c957509a4b688c12c324f73bb
Kushal <sennaboy001@gmail.com>
2016-04-11 23:57:39 +0530

    Update page_replacement.py

--- page_replacement.py
+++ page_replacement.py
@@ -1,1 +1,20 @@
+#God bless India
+frames=[]
 
+def base():
+  for i in range(20):
+    ref.append(
+  while 1:
+    print(" 1: FIFO 2: LRU 3: Optimal 4: Second Chance ")
+    ch=int(input())
+    if ch==1:
+      print(" 1: FIFO on reference string 2:FIFO on reverse reference string 3:Show Belady's anamoly ")
+      sub_choice=int(input())
+      
+
+
+def user_input():
+  print(" How many frames do you wish to use ")
+  no_frames=int(input())
+  return no_frames
+  
